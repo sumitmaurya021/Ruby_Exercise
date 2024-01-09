b421d2e9f7dea6667186fc34140547b7a1e88d04
sumit <mauryasumit222@gmail.com>
2024-01-09 18:56:47 +0530

    [9 Jan] [Ex 6] ["Fixing Problems"]

--- exercise_6.rb
+++ exercise_6.rb
@@ -1,14 +1,15 @@
 # Write a function to find the maximum element in an array.
 
-def find_max_number(number)
-  number = number.split(" ")
-  count = 0
-  number.each do
-    count += 1
- end
- puts count
+num = [10,20,40,100]
+def find_max_number(arr)
+  max = arr[0]
+
+  for value in arr
+    if value > max
+      max = value
+    end
+  end
+  return max
 end
 
-print "Enter A String : "
-input = gets.chomp.to_s
-puts find_max_number(input)
+puts find_max_number(num)
